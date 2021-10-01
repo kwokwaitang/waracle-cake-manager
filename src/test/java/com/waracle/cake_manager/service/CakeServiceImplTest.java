c121c51ed079428085ebbe9b34fd43c3ed4a0ec8
Kwok Wai Tang <kwok.tang68@gmail.com>
2021-10-01 15:41:42 +0100

    Updated unit tests for CakeServiceImpl.java

--- src/test/java/com/waracle/cake_manager/service/CakeServiceImplTest.java
+++ src/test/java/com/waracle/cake_manager/service/CakeServiceImplTest.java
@@ -1,25 +1,29 @@
 package com.waracle.cake_manager.service;
 
 import com.waracle.cake_manager.dto.CakeDto;
+import com.waracle.cake_manager.form.NewCakeDetails;
 import com.waracle.cake_manager.model.Cake;
+import com.waracle.cake_manager.model.NewCakeRequest;
+import com.waracle.cake_manager.model.NewCakeResponse;
 import com.waracle.cake_manager.repository.CakeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 class CakeServiceImplTest {
 
-    @Autowired
+    @Mock
     ModelMapper modelMapper = new ModelMapper();
 
     @Mock
@@ -34,7 +38,8 @@ class CakeServiceImplTest {
     }
 
     @Test
-    void getAvailableCakes() {
+    void getAvailableCakes_withCakes() {
+        // Set-up some entities
         Cake cake1 = new Cake();
         cake1.setEmployeeId(1L);
         cake1.setTitle("Lemon cheesecake");
@@ -47,9 +52,21 @@ class CakeServiceImplTest {
         cake2.setDescription("sponge with jam");
         cake2.setImageUrl("http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/recipe-image-legacy-id--1001468_10.jpg");
 
+        // Set-up some Dtos
+        CakeDto cakeDto1 = new CakeDto();
+        cakeDto1.setTitle("Lemon cheesecake");
+        cakeDto1.setDescription("A cheesecake made of lemon");
+        cakeDto1.setImage("https://s3-eu-west-1.amazonaws.com/s3.mediafileserver.co.uk/carnation/WebFiles/RecipeImages/lemoncheesecake_lg.jpg");
+
+        CakeDto cakeDto2 = new CakeDto();
+        cakeDto2.setTitle("victoria sponge");
+        cakeDto2.setDescription("sponge with jam");
+        cakeDto2.setImage("http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/recipe-image-legacy-id--1001468_10.jpg");
+
         Iterable<Cake> iterable = Arrays.asList(cake1, cake2);
 
         when(cakeRepository.findAll()).thenReturn(iterable);
+        when(modelMapper.map(any(Cake.class), eq(CakeDto.class))).thenReturn(cakeDto1, cakeDto2);
 
         List<CakeDto> cakeDtos = serviceImplUnderTest.getAvailableCakes();
 
@@ -60,7 +77,55 @@ class CakeServiceImplTest {
     }
 
     @Test
-    void addCakes() {
-        // TODO
+    void getAvailableCakes_withNoCakes() {
+        when(cakeRepository.findAll()).thenReturn(Collections.emptyList());
+
+        List<CakeDto> cakeDtos = serviceImplUnderTest.getAvailableCakes();
+        assertTrue(cakeDtos.isEmpty());
+    }
+
+    @Test
+    void addCake() {
+        NewCakeRequest newCakeRequest = new NewCakeRequest();
+        newCakeRequest.setTitle("abc");
+        newCakeRequest.setDescription("def");
+        newCakeRequest.setImageUrl("http://ghi.com");
+
+        Cake cake = new Cake();
+        cake.setEmployeeId(21L);
+        cake.setTitle("abc");
+        cake.setDescription("def");
+        cake.setImageUrl("http://ghi.com");
+
+        when(modelMapper.map(any(NewCakeRequest.class), eq(Cake.class))).thenReturn(cake);
+        when(cakeRepository.save(any(Cake.class))).thenReturn(cake);
+
+        NewCakeResponse newCakeResponse = serviceImplUnderTest.addCake(newCakeRequest);
+
+        assertEquals(21L, newCakeResponse.getId());
+    }
+
+    @Test
+    void getNewCakeRequest_withNewCakeDetails() {
+        NewCakeDetails newCakeDetails = new NewCakeDetails();
+        newCakeDetails.setTitle("abc");
+        newCakeDetails.setDescription("def");
+        newCakeDetails.setImage("https://some-nice-cake.com/with-lots-of-cream.gif");
+
+        NewCakeRequest mewNewCakeRequest = serviceImplUnderTest.getNewCakeRequest(newCakeDetails);
+
+        assertEquals(newCakeDetails.getTitle(), mewNewCakeRequest.getTitle());
+        assertEquals(newCakeDetails.getDescription(), mewNewCakeRequest.getDescription());
+        assertEquals(newCakeDetails.getImage(), mewNewCakeRequest.getImageUrl());
+    }
+
+    @Test
+    void getNewCakeRequest_withTitleDescImage() {
+        NewCakeRequest mewNewCakeRequest = serviceImplUnderTest.getNewCakeRequest("abc", "def", "https://some-nice" +
+                "-cake.com/with-lots-of-cream.gif");
+
+        assertEquals("abc", mewNewCakeRequest.getTitle());
+        assertEquals("def", mewNewCakeRequest.getDescription());
+        assertEquals("https://some-nice-cake.com/with-lots-of-cream.gif", mewNewCakeRequest.getImageUrl());
     }
 }
