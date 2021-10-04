98d9df2ba90863c90377e9f3758a4c88297b0464
Kwok Wai Tang <kwok.tang68@gmail.com>
2021-10-04 16:30:10 +0100

    Missing file from the previous commit

---
+++ src/main/java/com/waracle/cake_manager/config/WebSecurityConfig.java
@@ -0,0 +1,77 @@
+package com.waracle.cake_manager.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    static final String[] REQUESTS_NOT_NEEDING_AUTHENTICATION = {"/authenticate", "/register", "/new" +
+            "-cake-details", "/webjars/**", "/index.css", "/h2-console/**" };
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    private final UserDetailsService jwtUserDetailsService;
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+    private final PasswordEncoder bcryptEncoder;
+
+    public WebSecurityConfig(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                             UserDetailsService jwtUserDetailsService, JwtRequestFilter jwtRequestFilter,
+                             PasswordEncoder bcryptEncoder) {
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtUserDetailsService = jwtUserDetailsService;
+        this.jwtRequestFilter = jwtRequestFilter;
+        this.bcryptEncoder = bcryptEncoder;
+    }
+
+    /**
+     * Configure the AuthenticationManager so that it knows from where to load user details for matching credentials
+     * and also what encoder is used for the password
+     *
+     * @param authenticationManagerBuilder
+     * @throws Exception
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(jwtUserDetailsService).passwordEncoder(bcryptEncoder);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        // We don't need CSRF for this example...
+        httpSecurity.csrf().disable()
+                // Do not authenticate these particular requests...
+                .authorizeRequests().antMatchers(REQUESTS_NOT_NEEDING_AUTHENTICATION).permitAll()
+                // Whilst all other requests need to be authenticated...
+                .anyRequest().authenticated().and()
+                // Make sure we use stateless session; a session won't be used to store user's state.
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // Add a filter to validate the token with every request
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // To allow H2 to be displayed in a browser
+        httpSecurity.headers().frameOptions().disable();
+    }
+}
