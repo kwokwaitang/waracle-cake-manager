a49dbbafc3d8a2bc8faca5ac2fef6987e7f2713f
Kwok Wai Tang <kwok.tang68@gmail.com>
2022-04-15 15:00:10 +0100

    TODO add SWAGGER

--- src/main/java/com/waracle/cake_manager/CakeManagerApplication.java
+++ src/main/java/com/waracle/cake_manager/CakeManagerApplication.java
@@ -13,6 +13,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+// TODO add SWAGGER (see  https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api)
 @SpringBootApplication
 @EnableSwagger2
 public class CakeManagerApplication {
